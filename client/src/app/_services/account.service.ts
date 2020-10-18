b5c4705e37047d86eaad6282b7862f683a973203
Jemadean Dobrosielski <jemadean@gmail.com>
2020-10-17 22:34:06 -0700

    End of section 7

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -10,6 +10,7 @@ import { User } from '../_models/user';
 })
 export class AccountService {
   baseUrl = 'https://localhost:5001/api/';
+  //baseUrl = 'https://localhost:44309/api/';
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
